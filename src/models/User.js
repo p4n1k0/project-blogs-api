6af73019b0acf15e42fdeec4a86ba760f0f22e29
Tarick <gabre.tarick@gmail.com>
2022-09-25 18:33:33 -0300

    Req02.Cria model User

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    const userModel = sequelize.define('User', {
+        id: {
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+    },
+        {
+            tableName: 'users', underscored: true, timestamps: false
+        });
+    return userModel;
+};
