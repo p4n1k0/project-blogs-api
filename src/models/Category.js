35ae13ae956b0007add10938c222d9037b666b78
Tarick <gabre.tarick@gmail.com>
2022-09-30 14:56:10 -0300

    req07.Model Category

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Datatypes) => {
+    const categoryModel = sequelize.define('Category', {
+        id: {
+            primaryKey: true,
+            type: Datatypes.INTEGER
+        },
+        name: Datatypes.STRING
+    },
+        {
+            tableName: 'categories',
+            timestamps: false,
+            underscored: true,
+        });
+    return categoryModel;
+}
