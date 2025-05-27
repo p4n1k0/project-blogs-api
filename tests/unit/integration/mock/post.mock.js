f6544dcb7a43a7ed715559f147b3430b10bd17d9
p4n1k0 <gabre.tarick@gmail.com>
2025-05-27 15:33:02 -0300

    qse 90% de testes de integracao

--- tests/unit/integration/mock/post.mock.js
+++ tests/unit/integration/mock/post.mock.js
@@ -1,9 +1,49 @@
-const postCategory = {
-  "title": "Latest updates, August 1st",
-  "content": "The whole text for the blog post goes here in this key",
-  "categoryIds": [1, 2]
+const validTitle = 'Latest updates, August 1st';
+const validCategory = [1, 2];
+const validContent = 'The whole text for the blog post goes here in this key';
+
+const notTitle = {
+  title: undefined,
+  content: validContent,
+  categoryIds: [1, 2]
+};
+
+const notCategoryId = {
+  title: validTitle,
+  content: validContent,
+  categoryIds: {},
+};
+
+const addPost = {
+  title: validTitle,
+  content: validContent,
+  categoryIds: validCategory,
+};
+
+const postById = {
+  id: 1,
+  title: 'Post do Ano',
+  content: 'Melhor post do ano',
+  userId: 1,
+  published: '2011-08-01T19:58:00.000Z',
+  updated: '2011-08-01T19:58:51.000Z',
+  user: {
+    id: 1,
+    displayName: 'Lewis Hamilton',
+    email: 'lewishamilton@gmail.com',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg'
+  },
+  categories: [
+    {
+      id: 1,
+      name: 'Inovação'
+    }
+  ]
 };
 
 module.exports = {
-    postCategory
+  notTitle,
+  notCategoryId,
+  addPost,
+  postById
 };
--- tests/unit/integration/post.test.js
+++ tests/unit/integration/post.test.js
@@ -2,8 +2,8 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const app = require('../../../src/server');
-const { users, addUser } = require('./mock/user.mock');
-const { postCategory } = require('./mock/post.mock');
+const { users } = require('./mock/user.mock');
+const { notTitle, notCategoryId, addPost, postById } = require('./mock/post.mock');
 const { expect } = chai;
 
 
@@ -14,5 +14,59 @@ describe('Endpoint /post', () => {
     afterEach(() => {
         sinon.restore();
     });
-        
+
+    it('ao cadastrar um post sem titulo, retorne um erro', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).post('/post').send(notTitle).set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(400);
+        expect(data.body).to.be.deep.eq({ message: 'Some required fields are missing' });
+    });
+
+    it('ao cadastrar um post sem o campo categoryIds, retorne um erro', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).post('/post').send(notCategoryId).set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(400);
+        expect(data.body).to.be.deep.eq({ message: '"categoryIds" not found' });
+    });
+
+    it('cadastra um post com sucesso', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).post('/post').send(addPost).set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(201);
+        expect(data.body).to.be.deep.eq({ id: data.body.id, title: addPost.title, content: addPost.content, userId: data.body.userId, updated: data.body.updated, published: data.body.published });
+    });
+
+    it('buscando posts com sucesso', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).get('/post').send().set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(200);
+    });
+
+    it('buscando post pelo id com sucesso', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).get('/post/1').send().set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(200);
+        expect(data.body).to.be.deep.eq(postById);
+    });
+
+    it('ao buscar post pelo id inexistente, retorne um erro', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[0].email, password: users[0].password });
+        const data = await chai.request(app).get('/post/:id').send().set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(404);
+        expect(data.body).to.be.deep.eq({ message: 'Post does not exist' });
+    });
+
+    it('ao tentar atualizar psot sem autorização, retorne um erro', async () => {
+        const token = await chai.request(app).post('/login').send({ email: users[1].email, password: users[1].password });
+        const data = await chai.request(app).put('/post/1').send({ title: addPost.title, content: addPost.content }).set('Authorization', token.body.token);
+
+        expect(data.status).to.be.deep.eq(401);
+        expect(data.body).to.be.deep.eq({ message: 'Unauthorized user' });
+    });
 });
